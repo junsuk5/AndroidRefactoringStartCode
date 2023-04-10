1df176a84bb97e4f22ff29606ca426d219e94103
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-04-10 14:42:43 +0900

    refactor: ViewModel 적용하여 화면 회전시 UI 안 날아가게 수정

--- app/src/main/java/com/survivalcoding/imagesearchapp/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/imagesearchapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ProgressBar
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.GridLayoutManager
@@ -15,6 +16,8 @@ import com.survivalcoding.imagesearchapp.ui.PhotoAdapter
 class MainActivity : AppCompatActivity() {
     // TODO: 안전한 코드로 변경
     private lateinit var adapter: PhotoAdapter
+
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,44 +33,24 @@ class MainActivity : AppCompatActivity() {
         adapter = PhotoAdapter()
         recyclerView.adapter = adapter
 
+        updateUi(viewModel.photos)
+
         findViewById<ImageButton>(R.id.search_button).setOnClickListener {
             // TODO: 로딩 시작
             progressBar.isVisible = true
 
             // 사진 가져오기
-            val photos = getPhotos(queryEditText.text.toString())
+            viewModel.fetchPhotos(queryEditText.text.toString())
 
             // TODO: 오래 걸리는 처리
             Thread.sleep(2000)
 
             // TODO: 데이터 변경시 UI에 자동 반영
-            updateUi(photos)
+            updateUi(viewModel.photos)
 
             // TODO: 로딩 끝
             progressBar.isVisible = false
         }
-    }
-
-    // TODO: 실제로는 오래 걸리는 처리
-    private fun getPhotos(query: String): List<PhotoInfo> {
-        // TODO: UI 와 데이터 분리
-        return listOf(
-            PhotoInfo(
-                id = 0,
-                previewURL = "https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_org.jpg?61e575e8653e5920470a38d1482d7312/melon/optimize/90",
-                tags = "아이유, 여신"
-            ),
-            PhotoInfo(
-                id = 1,
-                previewURL = "https://image.bugsm.co.kr/album/images/500/40271/4027185.jpg",
-                tags = "아이유, 가수"
-            ),
-            PhotoInfo(
-                id = 2,
-                previewURL = "https://img.gqkorea.co.kr/gq/2022/08/style_63073140eea70.jpg",
-                tags = "아이유, 연기자"
-            ),
-        )
     }
 
     private fun updateUi(photos: List<PhotoInfo>) {
--- app/src/main/java/com/survivalcoding/imagesearchapp/MainViewModel.kt
+++ app/src/main/java/com/survivalcoding/imagesearchapp/MainViewModel.kt
@@ -1,7 +1,29 @@
 package com.survivalcoding.imagesearchapp
 
 import androidx.lifecycle.ViewModel
+import com.survivalcoding.imagesearchapp.data.PhotoInfo
 
-// TODO: UI와 데이터 분리
 class MainViewModel : ViewModel() {
+    var photos = emptyList<PhotoInfo>()
+
+    // TODO: 실제로는 오래 걸리는 처리
+    fun fetchPhotos(query: String) {
+        photos = listOf(
+            PhotoInfo(
+                id = 0,
+                previewURL = "https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_org.jpg?61e575e8653e5920470a38d1482d7312/melon/optimize/90",
+                tags = "아이유, 여신"
+            ),
+            PhotoInfo(
+                id = 1,
+                previewURL = "https://image.bugsm.co.kr/album/images/500/40271/4027185.jpg",
+                tags = "아이유, 가수"
+            ),
+            PhotoInfo(
+                id = 2,
+                previewURL = "https://img.gqkorea.co.kr/gq/2022/08/style_63073140eea70.jpg",
+                tags = "아이유, 연기자"
+            ),
+        )
+    }
 }
