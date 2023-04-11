4a7713daeddccd0a3042e32f49d5809fb93699ee
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-04-11 16:45:00 +0900

    이미지 검색앱 UI를 컴포즈로 작성 완료

--- app/src/main/java/com/survivalcoding/imagesearchapp/ui/compose/ImageSearchScreen.kt
+++ app/src/main/java/com/survivalcoding/imagesearchapp/ui/compose/ImageSearchScreen.kt
@@ -1,30 +1,32 @@
 package com.survivalcoding.imagesearchapp.ui.compose
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.survivalcoding.imagesearchapp.ui.MainViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 
 @Composable
 fun ImageSearchScreen(
     viewModel: MainViewModel = viewModel()
 ) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
     var query by rememberSaveable { mutableStateOf("") }
 
     Scaffold(
@@ -50,7 +52,9 @@ fun ImageSearchScreen(
                     .padding(16.dp),
                 trailingIcon = {
                     IconButton(
-                        onClick = {}
+                        onClick = {
+                            viewModel.fetchPhotos(query)
+                        }
                     ) {
                         Icon(
                             Icons.Default.Search,
@@ -60,14 +64,29 @@ fun ImageSearchScreen(
                 }
             )
 
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                modifier = Modifier.padding(16.dp)
-            ) {
-                items(listOf(1, 2, 3, 4)) { photo ->
-                    Text(text = "$photo")
+            if (state.isProgress) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    CircularProgressIndicator()
+                }
+            } else {
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    items(state.photos) { photo ->
+                        AsyncImage(
+                            model = photo.previewURL,
+                            contentDescription = photo.tags,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.aspectRatio(1f)
+                                .clip(RoundedCornerShape(10.dp))
+                        )
+                    }
                 }
             }
         }
